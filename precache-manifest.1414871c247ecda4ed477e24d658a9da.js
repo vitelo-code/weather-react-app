ebb08a6d272b22a72494958c23ad37440c535690
heliosb <roher86@gmail.com>
2023-05-16 19:29:06 -0600

    Updates

---
+++ precache-manifest.1414871c247ecda4ed477e24d658a9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "999a8f9ce5fc50ee4768",
+    "url": "/weather-react-app/static/css/main.c76bfac9.chunk.css"
+  },
+  {
+    "revision": "999a8f9ce5fc50ee4768",
+    "url": "/weather-react-app/static/js/main.999a8f9c.chunk.js"
+  },
+  {
+    "revision": "f04ddf66804a86d7ff92",
+    "url": "/weather-react-app/static/js/1.f04ddf66.chunk.js"
+  },
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "e1233d3feeb26e899b391b32b4015122",
+    "url": "/weather-react-app/static/media/rainy.e1233d3f.jpg"
+  },
+  {
+    "revision": "416c1341d7106188176d73177a326ac8",
+    "url": "/weather-react-app/index.html"
+  }
+];
